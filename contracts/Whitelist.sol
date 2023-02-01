d36af4255172d939bd385d4ba8da01e081598ba3
Appo <103305611+0xAppo@users.noreply.github.com>
2023-02-01 01:59:38 -0500

    added whitelist and finished documentation

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Indentifier: BSD-3-Clause
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Whitelist is Ownable {
+    mapping(address => bool) public isWhitelisted;
+
+    function updateWhitelist(address _address, bool _isActive) external onlyOwner {
+        isWhitelisted[_address] = _isActive;
+    }
+
+    function getWhitelisted(address _address) external view returns (bool) {
+        return isWhitelisted[_address];
+    }
+}
--- contracts/plvGLPOracle.sol
+++ contracts/plvGLPOracle.sol
@@ -1,19 +1,18 @@
 //SPDX-License-Identifier: BSD-3-Clause
 pragma solidity 0.8.17;
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Interfaces/GLPManagerInterface.sol";
 import "./Interfaces/plvGLPInterface.sol";
 import "./Interfaces/ERC20Interface.sol";
+import "./Whitelist.sol";
 
-//TODO: implement moving average similar to Uniswap v2?
-//TODO: finish documentation
 //TODO: optimize integer sizes for gas efficiency?
 
-/** @title Oracle for Plutus Vault GLP employing TWAP calculations for pricing
+/** @title Oracle for Plutus Vault GLP employing moving average calculations for pricing
     @author Lodestar Finance & Plutus DAO
-    @notice This contract uses a simple cumulative TWAP function. It is more resistant to manipulation
-    but reported prices are less fresh over time.
+    @notice This contract uses a moving point moving average calculation to report a plvGLP/GLP exchange
+    rate. The "window size" is adjustable to allow for flexibility in calculation parameters. The price
+    returned from the getPlvGLPPrice function is denominated in USD wei.
 */
 contract plvGLPOracle is Ownable {
     uint256 public averageIndex;
@@ -22,6 +21,7 @@ contract plvGLPOracle is Ownable {
     address public GLP;
     address public GLPManager;
     address public plvGLP;
+    address public whitelist;
 
     uint256 private constant BASE = 1e18;
     uint256 private constant DECIMAL_DIFFERENCE = 1e6;
@@ -35,23 +35,14 @@ contract plvGLPOracle is Ownable {
 
     IndexInfo[] public HistoricalIndices;
 
-    event IndexAlert(
-        uint256 previousIndex,
-        uint256 possiblyBadIndex,
-        uint256 timestamp
-    );
-
+    event IndexAlert(uint256 previousIndex, uint256 possiblyBadIndex, uint256 timestamp);
     event updatePosted(uint256 averageIndex, uint256 timestamp);
 
-    constructor(
-        address _GLP,
-        address _GLPManager,
-        address _plvGLP,
-        uint256 _windowSize
-    ) {
+    constructor(address _GLP, address _GLPManager, address _plvGLP, address _whitelist, uint256 _windowSize) {
         GLP = _GLP;
         GLPManager = _GLPManager;
         plvGLP = _plvGLP;
+        whitelist = _whitelist;
         windowSize = _windowSize;
         uint256 index = getPlutusExchangeRate();
         require(index > 0, "First index cannot be zero.");
@@ -59,6 +50,10 @@ contract plvGLPOracle is Ownable {
         HistoricalIndices.push(IndexInfo(block.timestamp, index));
     }
 
+    /**
+        @notice Pulls requisite data from GLP contract to calculate the current price of GLP
+        @return Returns the price of GLP denominated in USD wei.
+     */
     function getGLPPrice() public view returns (uint256) {
         //retrieve the minimized AUM from GLP Manager Contract
         uint256 glpAUM = GLPManagerInterface(GLPManager).getAum(false);
@@ -69,6 +64,10 @@ contract plvGLPOracle is Ownable {
         return price;
     }
 
+    /**
+        @notice Pulls requisite data from Plutus Vault contract to calculate the current exchange rate.
+        @return Returns the current plvGLP/GLP exchange rate directly from Plutus vault contract.
+     */
     function getPlutusExchangeRate() public view returns (uint256) {
         //retrieve total assets from plvGLP contract
         uint256 totalAssets = plvGLPInterface(plvGLP).totalAssets();
@@ -79,6 +78,11 @@ contract plvGLPOracle is Ownable {
         return exchangeRate;
     }
 
+    /**
+        @notice Computes the moving average over a period of "windowSize". For the initialization period,
+        the average is computed over the length of the indices array.
+        @return Returns the moving average of the index over the specified window.
+     */
     function computeAverageIndex() public returns (uint256) {
         uint256 latestIndexing = HistoricalIndices.length - 1;
         uint256 sum;
@@ -98,13 +102,21 @@ contract plvGLPOracle is Ownable {
         }
     }
 
+    /**
+        @notice Returns the value of the previously accepted exchange rate.
+     */
     function getPreviousIndex() public view returns (uint256) {
         uint256 previousIndexing = HistoricalIndices.length - 1;
-        uint256 previousIndex = HistoricalIndices[previousIndexing]
-            .recordedIndex;
+        uint256 previousIndex = HistoricalIndices[previousIndexing].recordedIndex;
         return previousIndex;
     }
 
+    /**
+        @notice Checks the currently reported exchange rate against the last accepted exchange rate.
+        Requested updates are compared against a range of +/- 1% of the previous exchange rate.
+        @param currentIndex the currently reported index from Plutus to check swing on
+        @return returns TRUE if requested update is within the bounds of maximum swing, returns FALSE otherwise.
+     */
     function checkSwing(uint256 currentIndex) public returns (bool) {
         uint256 previousIndex = getPreviousIndex();
         uint256 allowableSwing = (previousIndex * MAX_SWING) / BASE;
@@ -118,13 +130,14 @@ contract plvGLPOracle is Ownable {
     }
 
     /**
-        @notice Update the current, cumulative and average indices when required conditions are met
+        @notice Update the current, cumulative and average indices when required conditions are met.
         If the price fails to update, the posted price will fall back on the last previously 
         accepted average index.
         @dev we only ever update the index if requested update is within +/- 1% of previously accepted
-        index and update threshold has been reached. Revert otherwise.
+        index and update threshold has been reached.
      */
-    function updateIndex() public onlyOwner {
+    function updateIndex() external {
+        require(Whitelist(whitelist).isWhitelisted(msg.sender), "NOT_AUTHORIZED");
         uint256 currentIndex = getPlutusExchangeRate();
         uint256 previousIndex = getPreviousIndex();
         bool indexCheck = checkSwing(currentIndex);
@@ -140,6 +153,11 @@ contract plvGLPOracle is Ownable {
         }
     }
 
+    /**
+        @notice Computes the TWAP price of plvGLP based on the current price of GLP and moving average of the
+        plvGLP/GLP exchange rate.
+        @return Returns the TWAP price of plvGLP denominated in USD wei.
+     */
     function getPlvGLPPrice() external view returns (uint256) {
         uint256 glpPrice = getGLPPrice();
         uint256 plvGlpPrice = (averageIndex * glpPrice) / BASE;
@@ -148,8 +166,47 @@ contract plvGLPOracle is Ownable {
 
     //* ADMIN FUNCTIONS */
 
-    //TODO:
-    //transferOwnership
-    //update required addresses
-    //update params
+    event newGLPAddress(address oldGLPAddress, address newGLPAddress);
+    event newGLPManagerAddress(address oldManagerAddress, address newManagerAddress);
+    event newPlvGLPAddress(address oldPlvGLPAddress, address newPlvGLPAddress);
+    event windowSizeUpdated(uint256 oldWindowSize, uint256 newWindowSize);
+
+    /**
+        @notice Admin function to update the address of GLP, restricted to only be usable by the contract owner.
+     */
+    function _updateGlpAddress(address _newGlpAddress) external onlyOwner {
+        address oldGLPAddress = GLP;
+        GLP = _newGlpAddress;
+        emit newGLPAddress(oldGLPAddress, GLP);
+    }
+
+    /**
+        @notice Admin function to update the address of the GLP Manager Contract, restricted to only be 
+        usable by the contract owner.
+     */
+    function _updateGlpManagerAddress(address _newGlpManagerAddress) external onlyOwner {
+        address oldManagerAddress = GLPManager;
+        GLPManager = _newGlpManagerAddress;
+        emit newGLPManagerAddress(oldManagerAddress, GLPManager);
+    }
+
+    /**
+        @notice Admin function to update the address of plvGLP, restricted to only be usable by the 
+        contract owner.
+     */
+    function _updatePlvGlpAddress(address _newPlvGlpAddress) external onlyOwner {
+        address oldPlvGLPAddress = plvGLP;
+        plvGLP = _newPlvGlpAddress;
+        emit newPlvGLPAddress(oldPlvGLPAddress, plvGLP);
+    }
+
+    /**
+        @notice Admin function to update the moving average window size, restricted to only be usable 
+        by the contract owner.
+     */
+    function _updateWindowSize(uint256 _newWindowSize) external onlyOwner {
+        address oldWindowSize = windowSize;
+        windowSize = _newWindowSize;
+        emit windowSizeUpdated(oldWindowSize, windowSize);
+    }
 }
